b9fc191efa65a1d37e6585a2f6dd5adf0d568cf6
ADMIN1 <ADMIN1@192.168.1.17>
2019-12-20 16:24:55 +0530

    first

---
+++ Hello/src/org/servlet/HelloServlet.java
@@ -0,0 +1,36 @@
+package org.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet("/HelloServlet")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		// response.getWriter().append("Served at: ").append(request.getContextPath());
+	
+	System.out.println("HELLO JAVA \n");
+	response.getWriter().println("<h1>TEST MY Servlet </h1>");
+	}
+
+}
